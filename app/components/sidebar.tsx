604fbf51cad6b16999f9b0e15e3eca0c016339bb
tianyu365 <1807519653@qq.com>
2024-11-02 19:55:47 +0800

    更新 sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -230,8 +230,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="摩卡AI"
+        subTitle="我是摩卡AI"
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
